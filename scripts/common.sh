be476365afa6c24401c1fcdc6367a1ab2115352d
Stuart Pernsteiner <spernsteiner@galois.com>
2021-05-28 15:43:59 -0700

    add scripts/run_ffmpeg

--- scripts/common.sh
+++ scripts/common.sh
@@ -28,23 +28,6 @@ clean_picolibc() {
 }
 
 
-build_grit() {
-    build_llvm_passes
-    build_picolibc
-    (
-        cd "$cc_dir/grit"
-        fromager/build.sh microram
-    )
-}
-
-clean_grit() {
-    rm -rf \
-        "$cc_dir/grit/build" \
-        "$cc_dir/grit/driver-link.ll" \
-        "$cc_dir/grit/"lib*.a
-}
-
-
 build_microram() {
     (
         cd "$cc_dir/MicroRAM"
@@ -72,6 +55,24 @@ clean_witness_checker() {
 }
 
 
+# Examples
+
+build_grit() {
+    build_llvm_passes
+    build_picolibc
+    (
+        cd "$cc_dir/grit"
+        fromager/build.sh microram
+    )
+}
+
+clean_grit() {
+    rm -rf \
+        "$cc_dir/grit/build" \
+        "$cc_dir/grit/driver-link.ll" \
+        "$cc_dir/grit/"lib*.a
+}
+
 run_grit() {
     build_grit
     build_microram
@@ -90,6 +91,50 @@ run_grit() {
         cd "$cc_dir/out/grit"
         ../../witness-checker/target/release/cheesecloth \
             grit.cbor --stats --zkif-out zkif \
+            2>&1 | tee witness-checker.log
+    )
+}
+
+
+build_ffmpeg() {
+    build_llvm_passes
+    build_picolibc
+    (
+        cd "$cc_dir/ffmpeg"
+        [ -f Makefile ] || CVE-2013-0864/configure.sh
+        DRIVER_CFLAGS='-DSILENT' CVE-2013-0864/build.sh microram
+    )
+}
+
+clean_grit() {
+    make -C $cc_dir/ffmpeg clean
+
+    rm -rf \
+        "$cc_dir/ffmpeg/build" \
+        "$cc_dir/ffmpeg/driver-link.ll"
+}
+
+run_ffmpeg() {
+    build_ffmpeg
+    build_microram
+    build_witness_checker
+    mkdir -p "$cc_dir/out/ffmpeg"
+    (
+        cd "$cc_dir/MicroRAM"
+        stack run compile -- \
+            --from-llvm ../ffmpeg/driver-link.ll \
+            80000 \
+            --priv-segs 6800 \
+            -o ../out/ffmpeg/ffmpeg.cbor \
+            --verbose \
+            2>&1 | tee ../out/ffmpeg/microram.log
+    )
+    (
+        cd "$cc_dir/out/ffmpeg"
+        # Build the circuit but don't enable ZKIF output, since it's very
+        # expensive.
+        ../../witness-checker/target/release/cheesecloth \
+            ffmpeg.cbor --stats \
             2>&1 | tee witness-checker.log
     )
 }
